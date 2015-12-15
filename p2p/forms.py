613f142efd71e4ba4a2fd2924ecc4ef2b95f93ed
kutuhal <sdgogikar@gmail.com>
2015-12-15 07:05:31 +0530

    set the initial values in the forms.py

--- p2p/forms.py
+++ p2p/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class P2PForm(forms.Form):
     ITEM_TYPES = ( ('Expense','Expense'), ('Inventory','Inventory'))
     YES_OR_NO = ((True, 'Yes'), (False, 'No'))
-    item_type = forms.CharField(max_length=20, widget = forms.Select(choices=ITEM_TYPES))
-    period_end_accrual = forms.BooleanField(widget = forms.Select (choices= YES_OR_NO))
-    allow_recon_accounting =  forms.BooleanField(widget = forms.Select(choices= YES_OR_NO))
+    item_type = forms.CharField(max_length=20, widget = forms.Select(choices=ITEM_TYPES), initial='Expense')
+    period_end_accrual = forms.BooleanField(widget = forms.Select (choices= YES_OR_NO), initial=False)
+    allow_recon_accounting =  forms.BooleanField(widget = forms.Select(choices= YES_OR_NO), initial=False)
